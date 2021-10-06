0d7032f8b3d763beb39871112b239fcd9acae41b
Andr-Shch <sensor121@gmail.com>
2021-10-06 22:56:56 +0300

    add stack

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
  const newItem = document.querySelector('.create-queue-item__content');
  const startBtn = document.querySelector('#startBtn')
  const queue = document.querySelector('.queue')
+ const stack = document.querySelector('.stack__field')
  const form = document.getElementById('form')
  const mentalAge = 16 
 
 
  let queueList=[]
- 
+ let stackList=[]
+
  const createQueue=()=>{      // create divs for queue from queueList(array)
     queue.innerHTML=queueList.map((el, ind)=>`<div class="queue__item"><span>${ind+1}.</span> ${el}</div>`).join` `
  } 
@@ -23,15 +25,12 @@
    sessionStorage.setItem( 'data', JSON.stringify(value) )
  }
 
-
-
-
  
  form.onsubmit = (e) =>{
     e.preventDefault();
- if(queueList.length>=mentalAge){     //validation for quantity
+   if(queueList.length>=mentalAge){     //validation for quantity
       alert('Error: Queue frame too large. Strat task to free space, only 16 slots available ')
-  } else{
+   } else{
     queueList.push(newItem.value)
     createQueue()
     newItem.value = '';
@@ -40,8 +39,21 @@
  }
 
  startBtn.addEventListener('click', ()=>{
-     queueList.shift()
+    setItemInStack ()
+    queueList.shift()
      createQueue()
      setInSessinStorage(queueList)
  })
-   
+
+ function setItemInStack () {   
+      let onExit  = queueList[0];
+      if (onExit){
+      stackList.push(onExit);
+      
+      stack.innerHTML=stackList.map((el, ind)=>`<div class="stack-field__item"> ${el}</div>`).join` `
+      setTimeout(() => {
+        stackList.pop()
+        stack.innerHTML=stackList.map((el, ind)=>`<div class="stack-field__item"> ${el}</div>`).join` `
+      }, 1000);
+    }
+    }
